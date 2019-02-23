1166110efbcfd7096b5aeccf5da7e16c8033eb90
ttjibu <47848882+ttjibu@users.noreply.github.com>
2019-02-23 08:57:39 +0800

    Update selection_sorting.cpp

--- selection_sorting.cpp
+++ selection_sorting.cpp
@@ -4,8 +4,9 @@ using namespace std;
 string Element::sort_name{"selection_sorting "};
 
 void Element::sort_method() {
-	int j,k,i,m=-1000;
+	int j,k,i,m,M=-1000;
 	for(j=1; j<=data.size(); j++) {
+		m=M;
 		for(k=j;k<=data.size();k++){
 			if(m<[data[k]]){
 				m=data[k];i=k;
